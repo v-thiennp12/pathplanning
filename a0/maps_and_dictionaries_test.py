70f8992e2e6f1f62aab70ecfb7d949d42520a28d
Nguyễn Phúc Thiện <v.thiennp12@vinai.io>
2023-06-15 17:58:04 +0700

    exercise a0 OK

--- a0/maps_and_dictionaries_test.py
+++ a0/maps_and_dictionaries_test.py
@@ -1,9 +1,12 @@
 # To pass the test, your code should output "Passed All Dictionary Tests!"
 # If you get any other error other than the one printed below, google to figure out what you did. Most likely error to encounter is a Key Error (means key doesn't exist in dictionary).
+
 def test(value, answer):
     if value != answer:
         print('Failed - ' + str(value) + ' should be ' + str(answer))
-        exit()
+    else:
+        print('Success - ' + str(value) + ' == ' + str(answer))
+    
 print('')
 # Create a blank dictionary called employees
 employees = {}
@@ -13,25 +16,38 @@ employees = {}
 # Key: 1  Value: 'Andre'
 # Key: 2  Value: 'Ted'
 # Key: 3  Value: 'Alex'
+employees['0'] = 'Ryan'
+employees['1'] = 'Andre'
+employees['2'] = 'Ted'
+employees['3'] = 'Alex'
 
+print(employees)
 
 # TEST
-test(employees[0], 'Ryan')
+test(employees['0'], 'Ryan')
 
-# Sort the employees in alphabetical order.
-sorted_employees = sorted(employees)
-test(sorted_employees[0], 3)
-test(sorted_employees[1], 1)
-test(sorted_employees[2], 0)
-test(sorted_employees[3], 2)
+# # Sort the employees in alphabetical order.
+sorted_employees = sorted(employees, key=employees.get, reverse=False)
+print(sorted_employees)
+print(employees[sorted_employees[0]])
+print(employees[sorted_employees[1]])
+print(employees[sorted_employees[2]])
+print(employees[sorted_employees[3]])
+
+test(sorted_employees[0], '3')
+test(sorted_employees[1], '1')
+test(sorted_employees[2], '0')
+test(sorted_employees[3], '2')
 
 
-# Pop employee number 2 and store name in "name".
-name = ''
+# # Pop employee number 2 and store name in "name".
+name = employees['2']
 test(name, 'Ted')
 
+employees.pop('2')
+
 try: 
-    employees[2]
+    employees['2']
 except: 
     print('Passed All Dictionary Tests!')
     exit()
